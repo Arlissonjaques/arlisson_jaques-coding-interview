39fe2d01907eed3dfa8137fc51f8bb36ebc90348
Arlisson Jaques <arlissonjaques@gmail.com>
2026-02-04 15:28:25 -0500

    feat: tenta implementar paginacao

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,17 @@
 class TweetsController < ApplicationController
   def index
-    user = params[:user_username].present? ? Tweet.by_user(params[:user_username]) : Tweet.all
-    
-    render json: user 
+    @tweet = params[:user_username].present? ? Tweet.by_user(params[:user_username]) : Tweet.all
+
+    render json: paginate(@tweet) 
+  end
+
+  private
+
+  def paginate
+    @tweet = @tweet.order(created_at: :asc).limit(paginate_params(:per_page))
+  end
+
+  def paginate_params
+    params.permit(:page, :per_page).with_default(page: 1, per_page: 5)
   end
 end
